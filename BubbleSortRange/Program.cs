540cd7ca033b7f423bc86f25a70e604e046cc23b
kichapinv <kichapinv@gmail.com>
2023-03-16 19:11:30 +0700

    edits01

--- BubbleSortRange/Program.cs
+++ BubbleSortRange/Program.cs
@@ -1,22 +1,39 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
+    public static Random random = new Random();
+
     private static void Main(string[] args)
     {
-        var random = new Random();
         int[] firstArray = new int[] { 4, 3, 2, 0, 1, 5, 7, 6, 9, 8 };
+        int[] secondArray = new int[random.Next(100)];
+        
 
-        Console.WriteLine("Lets sort a range of array by bubblesort");
+        Console.WriteLine("Lets sort a range of array by bubblesort. We will make two random arrays and sort them.");
+        
         Console.Write("First array: ");
         WriteAnArray(firstArray);
+        Console.WriteLine("Insert first and second border");
+        var firstBorder = Console.ReadKey(true).Key;
+        var secondBorder = Console.ReadKey(true).Key;
         WriteAnArray(BubbleSortRange(firstArray, 2, 6));
         
-        int[] newArray = new int[random.Next(100)];
-        for (int i = 0; i < newArray.Length; i++)
-        {
-            newArray[i] = random.Next(100);
-        }
+        
 
-        WriteAnArray(BubbleSortRange(newArray, 1, 2));
+        WriteAnArray(BubbleSortRange(secondArray, 1, 2));
+    }
+
+    public static int[] MakeRandomArray()
+    {
+        Console.Write("Insert the length of array: ");
+        char lengthOfArray = Console.ReadKey(true).KeyChar;
+        int[] array = new int[lengthOfArray];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(100);
+        }
+        return array;
     }
 
     public static int[] BubbleSortRange(int[] array, int left, int right)
@@ -34,8 +51,6 @@
 
     public static void WriteAnArray(int[] array)
     {
-        Console.Write("Array length: ");
-        Console.WriteLine(array.Length);
         for (int i = 0; i < array.Length; i++)
         {
             Console.Write(array[i] + " ");
